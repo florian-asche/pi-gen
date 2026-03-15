6d5c99a7965d48eb410f3e8f831a5d58ef511618
Florian Asche <github@florian-asche.de>
2026-03-15 19:59:08 +0100

    add workaround for image path

--- build-docker.sh
+++ build-docker.sh
@@ -83,14 +83,8 @@ fi
 BUILD_OPTS="$(echo "${BUILD_OPTS:-}" | sed -E 's@\-c\s?([^ ]+)@-c /config@')"
 
 # Check the arch of the machine we're running on. If it's 64-bit, use a 32-bit base image instead
-case "$(uname -m)" in
-  x86_64|aarch64)
-    BASE_IMAGE=i386/debian:trixie
-    ;;
-  *)
-    BASE_IMAGE=debian:trixie
-    ;;
-esac
+BASE_IMAGE=debian:trixie
+
 ${DOCKER} build --build-arg BASE_IMAGE=${BASE_IMAGE} -t pi-gen "${DIR}"
 
 if [ "${CONTAINER_EXISTS}" != "" ]; then
